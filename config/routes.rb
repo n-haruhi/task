b3f851e44af4c87321ea76efa65ae9ad9a51afe3
n-haruhi <hr23nakanishi@gmail.com>
2023-12-23 03:06:44 +0000

    [Add]ヘッダー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
+  root to: 'homes#top'
+
+  get '/top' => 'homes#top'
+  get '/about' => 'homes#about'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '', to: 'todos#index'
   get 'todos', to: 'todos#index'
   post 'todos', to: 'todos#addTodo'
   delete 'todos/:id', to: 'todos#deleteTodo'
