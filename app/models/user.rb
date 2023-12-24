ade6d210abc21af28264a0504cc3be414f6c54e1
n-haruhi <hr23nakanishi@gmail.com>
2023-12-24 16:33:03 +0000

    [Update]トップページbutton追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name,presence:true
-  validates :email,presence:true
-  validates :password, presence:true
+  has_many :todos, dependent: :destroy
+
+  validates :name, presence:true, uniqueness: true,  length: { minimum: 2 }
+  validates :email, presence:true, uniqueness: true
+  validates :password, presence:true, uniqueness: true, length: { minimum: 6 }
 
 end
