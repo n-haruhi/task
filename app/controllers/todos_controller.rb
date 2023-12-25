6bd344bc3b8f7356b1254af9db9f6ae9d01037cd
n-haruhi <hr23nakanishi@gmail.com>
2023-12-25 14:29:13 +0000

    [Update]アクション追加

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -10,6 +10,24 @@ class TodosController < ApplicationController
     redirect_to '/todos'
   end
 
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      flash[:success] = "Todo was successfully updated."
+      redirect_to todo_path(@todo.id)
+    else
+      render :new
+    end
+  end
+
   def deleteTodo
     todo = Todo.find(params[:id])
     todo.destroy
